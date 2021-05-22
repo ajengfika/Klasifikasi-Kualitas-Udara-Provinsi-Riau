662123beac397703f6c421ef0005b9352b87352d
ajengfika <84580585+ajengfika@users.noreply.github.com>
2021-05-22 08:51:01 +0700

    Update klasifikasikualitasudara.py

--- klasifikasikualitasudara.py
+++ klasifikasikualitasudara.py
@@ -3,7 +3,6 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import streamlit as st
-import xlrd
 
 from sklearn.model_selection import train_test_split  
 from sklearn.svm import SVC  
